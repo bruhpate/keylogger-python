307792a3ca12a69158ccb508aebdfd007d613c2e
bruhpate <pietropatelli05@gmail.com>
2022-06-08 18:13:43 +0200

    remove useless modules

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,10 +1,9 @@
-from asyncio import sleep
 from lib import keyboard
 from datetime import datetime
 
 def recording():
     return keyboard.record(until=('escape + shift'))
-    
+
 def calcualteDataAndHour():
     tempTime = datetime.now()
     return str(tempTime.strftime("%Y-%m-%d %H:%M:%S"))
